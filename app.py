fd9b64f204c6bdd50587295df5d53e08613c3598
Bilal Sheikh <65639242+web-desire-creator@users.noreply.github.com>
2023-04-10 19:58:16 +0500

    compressed file

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
- import os
+import os
 from os import listdir
 from PIL import Image
+import shutil
+import cv2
 
 i=1
-folder_dir=os.getcwd()
-for images in os.listdir(folder_dir):
-    if (images.endswith(".jpg") or images.endswith(".jpeg")):
-        img_jpg=Image.open(images)
-        img_jpg=img_jpg.resize((128,128),Image.ANTIALIAS)
+# url = input("Add path of first file: ")
+url = os.getcwd()
+
+
+dir = url+"\\tiff"
+if not os.path.exists(dir): # if the directory does not exist
+    os.makedirs(dir) # make the directory
+else: 
+    shutil.rmtree(dir)
+    os.makedirs(dir)
+
+
+
+for images in os.listdir(url):
+    if (images.endswith(".jpg") or images.endswith(".jpeg") or images.endswith(".png")):
+        print(images)
+        img_jpg=Image.open(url+"\\"+images)
         rgb=img_jpg.convert("RGB")
-        rgb.save(os.getcwd()+"/tiff/"+str(i)+".tiff")
+        rgb.save(url+"\\tiff\\"+str(i)+".tiff")
+        image = cv2.imread(dir+"\\"+str(i)+".tiff")
+        cv2.imwrite(dir+"\\"+str(i)+".tiff", image, params=(cv2.IMWRITE_TIFF_COMPRESSION, 8))
+        print("Image Compressed Successfully")
+        im = Image.open(dir+"\\"+str(i)+".tiff")
+        resized_im = im.resize((round(im.size[0]*1), round(im.size[1]*1)))
+        resized_im.save(dir+"\\"+str(i)+".tiff")
         i=i+1
