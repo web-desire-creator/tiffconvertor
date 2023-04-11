0f17697f935d940b381173fc7f1756e6c6ab1414
Bilal Sheikh <65639242+web-desire-creator@users.noreply.github.com>
2023-04-11 11:55:38 +0500

    file size and image name updated

--- app.py
+++ app.py
@@ -19,18 +19,19 @@ else:
 
 for images in os.listdir(url):
     if (images.endswith(".jpg") or images.endswith(".jpeg") or images.endswith(".png")):
+        fileName = os.path.splitext(images)
         img_jpg=Image.open(url+"\\"+images)
         rgb=img_jpg.convert("RGB")
-        rgb.save(url+"\\tiff\\"+str(i)+".tiff")
-        image = cv2.imread(dir+"\\"+str(i)+".tiff")
-        cv2.imwrite(dir+"\\"+str(i)+".tiff", image, params=(cv2.IMWRITE_TIFF_COMPRESSION, 8))
-        file_name = dir+"\\"+str(i)+".tiff"
+        rgb.save(url+"\\tiff\\"+fileName[0]+".tiff")
+        image = cv2.imread(dir+"\\"+fileName[0]+".tiff")
+        cv2.imwrite(dir+"\\"+fileName[0]+".tiff", image, params=(cv2.IMWRITE_TIFF_COMPRESSION, 8))
+        file_name = dir+"\\"+fileName[0]+".tiff"
         file_stats = os.stat(file_name).st_size/1024
         if(file_stats>50):
-            while((file_stats/(0.8*0.8))>50):
-                im = Image.open(dir+"\\"+str(i)+".tiff")
+            while((file_stats/(0.8*0.8))>40):
+                im = Image.open(dir+"\\"+fileName[0]+".tiff")
                 resized_im = im.resize((round(im.size[0]*0.8), round(im.size[1]*0.8)))
-                resized_im.save(dir+"\\"+str(i)+".tiff")
+                resized_im.save(dir+"\\"+fileName[0]+".tiff")
                 file_stats=file_stats*0.8*0.8
             i=i+1
         else:
